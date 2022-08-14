d1c9660511027cc7e2880d564f70a49acbf09439
MarkBenedictMuyot <markmuyot@hotmail.com>
2022-08-13 21:17:31 -0400

    added alert with conditions for contact me page/ alert()

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// =========== about me function==================
+
 function scrollToTop() {
   window.scrollTo(0, 0);
+}
+
+// ===========Contact me Function ==================
+
+function showAlert() {
+  if (
+    document.getElementById("name").value &&
+    document.getElementById("email").value
+  ) {
+    alert("Submitted! I'm very excited to contact you soon!");
+  }
 }
