6218585292e56798cddd2d29896acbe10588afda
MarkBenedictMuyot <markmuyot@hotmail.com>
2022-08-16 16:34:13 -0400

    added fontface Arvo/ added structure of blogpage/ resized contact page/ added javascript function

--- script.js
+++ script.js
@@ -4,9 +4,23 @@ function scrollToTop() {
   window.scrollTo(0, 0);
 }
 
+function contactMeAlert() {
+  alert("Please contact me too see implementation of this project!");
+}
+
 // ===========Contact me Function ==================
 
 function showAlert() {
+  if (
+    document.getElementById("name").value == "" &&
+    document.getElementById("email").value == ""
+  ) {
+    alert("Please Enter Your Name and Email!");
+  } else if (document.getElementById("name").value == "") {
+    alert("Please Enter Your Name!");
+  } else if (document.getElementById("email").value == "") {
+    alert("Please Enter Your Email!");
+  }
   if (
     document.getElementById("name").value &&
     document.getElementById("email").value
